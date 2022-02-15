87ae208f1f644ef86e18af5c9e445b6b7eb7a88e
100rabh <saurabh.8189@gmail.com>
2022-02-15 10:09:51 +0530

    Create MostCommonWord.java

---
+++ MostCommonWord.java
@@ -0,0 +1,64 @@
+/****
+Most Common Word
+
+Solution
+Given a string paragraph and a string array of the banned words banned, return the most frequent word that is not banned. It is guaranteed there is at least one word that is not banned, and that the answer is unique.
+
+The words in paragraph are case-insensitive and the answer should be returned in lowercase.
+
+ 
+
+Example 1:
+
+Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
+Output: "ball"
+Explanation: 
+"hit" occurs 3 times, but it is a banned word.
+"ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph. 
+Note that words in the paragraph are not case sensitive,
+that punctuation is ignored (even if adjacent to words, such as "ball,"), 
+and that "hit" isn't the answer even though it occurs more because it is banned.
+Example 2:
+
+Input: paragraph = "a.", banned = []
+Output: "a"
+ 
+
+Constraints:
+
+1 <= paragraph.length <= 1000
+paragraph consists of English letters, space ' ', or one of the symbols: "!?',;.".
+0 <= banned.length <= 100
+1 <= banned[i].length <= 10
+banned[i] consists of only lowercase English letters.
+***/
+//Trie can be used for better space complexity
+class MostCommonWord {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        String maxS="";
+        if(paragraph != null){
+            
+            paragraph =paragraph.toLowerCase().replace("!"," ").replace("?"," ").replace("'"," ").replace(";"," ").replace("."," ").replace(","," ");
+            
+            for(String b1:banned){
+                paragraph=paragraph.replaceAll("\\b"+b1+"\\b", " ");
+            }
+            String[] p = paragraph.split(" ");
+            Map<String, Integer> m = new HashMap();
+            int max=0;
+            for(String s:p){
+                if(s!=" " && s!=""){
+                    int o= m.getOrDefault(s.toLowerCase(),0);
+                    if((o+1)>max){
+                        maxS=s.toLowerCase();
+                        max = o+1;
+                        
+                    }
+                    m.put(s.toLowerCase(),o+1);
+
+                }
+            }
+        }
+        return maxS;
+    }
+}
